e5f1cd8bc8cfa6f4cada76a4c0d1ce0d39ae7e09
gensc004 <gensc004@morris.umn.edu>
2016-03-02 21:48:08 -0600

    Removed futures

--- src/simple_search/experiment.clj
+++ src/simple_search/experiment.clj
@@ -14,7 +14,7 @@
   (for [searcher searchers
         p problems
         n (range num-replications)]
-    (let [answer (future (searcher p max-evals size))]
+    (let [answer (searcher p max-evals size)]
       {:searcher searcher
        :problem p
        :max-evals max-evals
@@ -28,7 +28,7 @@
              (:label (:problem result))
              (:max-evals result)
              (:run-number result)
-             (:score @(:answer result)))))
+             (:score (:answer result)))))
 
 ;; This really shouldn't be necessary, as I should have included the labels
 ;; in the maps when generated the problem files. Unfortunately I only just
